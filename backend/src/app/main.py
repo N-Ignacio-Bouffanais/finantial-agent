c8657c69116ae105afe2c6a03bbbe23a34540d26
Nicolas <nicolas.bouffanais.1999@gmail.com>
2025-02-05 00:30:42 -0300

    main.py

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,1 +1,14 @@
-import fastapi from FastApi
+from fastapi import FastAPI
+from app.api import auth, llm
+from app.config import settings
+
+app = FastAPI(title="Finantial Api Agent", description="This is an API for LLM langchain project")
+
+
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(llm.router, prefix="/ask", tags=["llm"])
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host=settings.HOST, port=settings.PORT)
