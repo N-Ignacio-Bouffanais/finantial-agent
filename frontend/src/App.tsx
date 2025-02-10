63f86c98f11a937ae438c9dd413738c1db538347
Nicolas <nicolas.bouffanais.1999@gmail.com>
2025-02-10 20:40:34 -0300

    tailwindcss hot fix vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
 
 function App() {
+
   return (
     <>
-      <div>
-        <h1>Welcome</h1>
-      </div>
+      <h1 className="text-3xl font-bold underline bg-amber-400">Hello world!</h1>
     </>
-  )
+  );
 }
 
 export default App
